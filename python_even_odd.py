ed60cc7b1285926741220088cc87c80f2a271d2b
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2018-06-10 17:41:53 -0400

    Add files via upload
    Just a simple separation of even and odd numbers from 1 to 50.

---
+++ python_even_odd.py
@@ -0,0 +1,11 @@
+even = []
+odd = []
+for value in range(1,50):
+    if value % 2 == 0:
+        even.append(value)
+    else:
+        odd.append(value)
+print('All even numbers are ' + str(even))
+print('All odd numbers are ' + str(odd))
+    
+    
