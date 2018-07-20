fdc46ee61e479e1d65521df054fe581a996177b3
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2018-07-19 22:41:12 -0400

    Add files via upload
    A very simple example of data analysis using the Matplotlib module in Python.  I used my own real life travel expense data for this example.  Enjoy and please challenge me on my work, I will greatly appreciate it! :)

---
+++ python_simple_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+daily_gas = [15,13,13.5,14.7,11.4]
+weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+
+plt.plot(weekdays,daily_gas)
+plt.title('My Average Daily cost of fuel')
+plt.xlabel('Daily drive')
+plt.ylabel('Average Daily Gas Consumption in Dollars')
+plt.show()
