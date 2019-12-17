96c4576f3efbb8a8a7e3ddbe42d6f882b81e2a96
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2019-12-17 11:39:59 -0500

    Update Median.py

--- MachineLearning/Median.py
+++ MachineLearning/Median.py
@@ -1,3 +1,7 @@
+#Now we are calculating which number is the middle. 
+#It's easy with a small array but with dozens of values it's harder to spot without doing the math.
+#Thankfully numpy will handle this again.
+
 import numpy as np
 import math as mt
 
@@ -6,4 +10,4 @@ daily_gas = [15,13,13.5,14.7,11.4]
 middle_number = np.median(daily_gas)
 x= mt.ceil(middle_number)
 print(x)
-#print(middle_number)
+
