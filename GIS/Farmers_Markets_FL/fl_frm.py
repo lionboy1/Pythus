eee295419b56ffe22ee6ca6a362c8877af3fa229
lionboy1 <39709290+lionboy1@users.noreply.github.com>
2020-01-30 15:56:30 -0500

    Update fl_frm.py
    Added more locations for after school programs providing food(currently commented out).  Also included location info on the pop-up to in addition to market name.

--- GIS/Farmers_Markets_FL/fl_frm.py
+++ GIS/Farmers_Markets_FL/fl_frm.py
@@ -1,4 +1,3 @@
-#Borrowed some ideas from https://www.kaggle.com/daveianhickey/how-to-folium-for-maps-heatmaps-time-analysis
 import folium
 import pandas as pd
 
@@ -9,23 +8,36 @@ locations = pd.read_csv('fl_state_frm.csv')
 locations['Lat'] = locations['Lat'].astype(float)
 locations['Lon'] = locations['Lon'].astype(float)
 locations['Result'] = locations['Result'].astype(str)
+locations['Location'] = locations['Location'].astype(str)
 
 #Maybe over kill but when variables are set for map, force data type conversion again
 long = pd.to_numeric(locations['Lon'], errors='coerce')
 lat = pd.to_numeric(locations['Lat'], errors='coerce')
 name = locations['Result'] = locations['Result'].astype(str)
+locale = locations['Location'] = locations['Location'].astype(str)
+info = name + '\n' +locale
 
 #A quick check to see the array list starting from position 0 and showing everything after to the end
-print(long.iloc[0:])
+#print(long.iloc[0:])
 
 #Iterate over the csv and populate the map with the data
 
-for index, (value1, value2, value3) in enumerate(zip(lat, long, name)):    
+for index, (value1, value2, value3) in enumerate(zip(lat, long, info)):    
     folium.Marker([value1, value2],
                   popup=value3,
                   icon=folium.Icon(color='green',icon='fa-leaf', prefix='fa') #use any font awesome icon
                  ).add_to(my_florida)
-                 
+    
+'''foodLocations = pd.read_csv('Afterschool_Meals_Program_Sites_2019.csv')
+foodLat = foodLocations['md_Latitude'].astype(float)
+foodLon = foodLocations['md_Longitude'].astype(float)
+foodName = foodLocations['Site_Name'].astype(str)
+for index, (value1, value2, value3) in enumerate(zip(foodLat, foodLon, foodName)):    
+    folium.Marker([value1, value2],
+                  popup=value3, 
+                  icon=folium.Icon(color='green',icon='fa-cutlery', prefix='fa'),
+                 ).add_to(my_florida)    
+'''                 
 #Now load the map
 
 my_florida
